96cb98d2b38063b5fc67e97d0d8191097312e4b8
Bastian Krohg <98895859+bastiankrohg@users.noreply.github.com>
2023-03-31 10:30:34 +0200

    First prototype new Comité Pascin website

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+//import React from 'react';
+//import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//import App from './App';
 
+/*
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+*/
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+/***********************/
+const gallery = document.getElementById('gallery');
+
+// Replace the URLs in the following array with your own photos
+const photos = [
+  'photo1.jpg',
+  'photo2.jpg',
+  'photo3.jpg',
+  // add more photos here...
+];
+
+// Loop through the array of photos and create image elements for each one
+photos.forEach(photo => {
+  const img = document.createElement('img');
+  img.src = photo;
+  img.alt = 'My photo gallery';
+  gallery.appendChild(img);
+});
+/***********************/
